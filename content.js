45371488bd57c76d0b3edc3368d086dd53c15cdd
Harry Boyd <hboyd255@gmail.com>
2024-06-15 20:13:35 +0100

    Added icons

--- content.js
+++ content.js
@@ -13,26 +13,40 @@ searchResultContainers = searchResultContainers.filter((container) => {
   return container.querySelector("h3");
 });
 
+// Limit the number of search result containers to 10.
+searchResultContainers = searchResultContainers.slice(0, 10);
+
 //TODO Remove this line
 console.log("searchResultContainers: ", searchResultContainers);
 
 // Loop through each search result container.
-searchResultContainers.forEach((container) => {
+searchResultContainers.forEach((container, index) => {
+  // Get the heading element.
+  let number = index + 1;
+
+  if (index == 9) {
+    number = 0;
+  }
+
   // Create an icon element.
   let icon = document.createElement("img");
 
+  // Set the path to the icon, based on the number.
+  let pathToIcon = "icons/icon_" + number + ".png";
+
   // Set the icon's attributes.
-  icon.src =
-    "https://ssl.gstatic.com/calendar/images/dynamiclogo_2020q4/calendar_15_2x.png"; //TODO Change the icon
+  icon.src = chrome.extension.getURL(pathToIcon); //TODO Change the icon
   icon.alt = "Icon";
 
   // Style the icon to match the text size
-  icon.style.width = "1.5em"; // Match the font size of the text
-  icon.style.height = "1.5em"; // Match the font size of the text
+  icon.style.width = "1.2em"; // Match the font size of the text
+  icon.style.height = "1.2em"; // Match the font size of the text
   icon.style.marginRight = "10px"; // Add some space between the icon and the text
   icon.style.verticalAlign = "middle"; // Vertically align the icon with the text
 
   // Get the heading element.
   Heading = container.querySelector("h3");
+
+  // Prepend the icon to the heading element.
   Heading.prepend(icon);
 });
